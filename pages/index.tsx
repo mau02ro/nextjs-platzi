acdbc156dc922eee2eec90c5e86569b94581acf0
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-08-28 14:12:56 -0430

    avo app

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
+import Layout from '@components/Layout/Layout'
+import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
+import ProductList from '@components/ProductList/ProductList'
+
+export const getServerSideProps = async () => {
+  const response = await fetch('api/avo')
+  const {data} = await response.json()
+
+
+  return {
+    props:{
+      productList: data
+    }
+  }
+}
+
+const HomePage = ({ productList }:{productList: TProduct[]}) => {
+
+ 
+
+  return (
+    <Layout>
+      <KawaiiHeader />
+      <ProductList products={productList} />
+    </Layout>
+  )
+}
+
+export default HomePage
